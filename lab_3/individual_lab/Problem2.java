f66975cb77173361aea13856e78e03fc0cce65d4
Carl Piercy <cpiercy111@gmail.com>
2017-09-11 11:23:16 -0700

    lab

--- lab_3/individual_lab/Problem2.java
+++ lab_3/individual_lab/Problem2.java
@@ -15,6 +15,97 @@ public class Problem2 extends Robot
     }
 
     public void carpetRooms(){
+        checkRoom();
+        checkRoom();
+        checkRoom();
+        checkRoom();
+        checkRoom();
+        checkRoom();
+        checkRoom();
+        checkRoom();
+    }
+    public void checkRoom() {
+        move();
+        turnLeft();
+        move();
+        if(frontWallClear() && leftWallClear() && rightWallClear()) {
+            putBeeper();
+            reposition();
+        }
+        else {
+           reposition(); 
+        }
+    }
+    public boolean frontWallClear() {
+        faceNorth();
+        if(frontIsClear()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public boolean leftWallClear() {
+        faceWest();
+        if(frontIsClear()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public boolean rightWallClear() {
+        faceEast();
+        if(frontIsClear()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void faceEast() {
+        if(facingWest()) {
+            turnRight();
+            turnRight();
+        }
+        if(facingNorth()) {
+            turnRight();
+        }
+        if(facingSouth()) {
+            turnLeft();
+        }
+    }
+    public void faceWest() {
+        if(facingNorth()) {
+            turnLeft();
+        }
+        if(facingEast()) {
+            turnLeft();
+            turnLeft();
+        }
+        if(facingSouth()) {
+            turnRight();
+        }
+    }
+    public void faceSouth() {
+        if(facingNorth()) {
+            
+        }
+        if(facingEast()) {
+            
+        }
+        if(facingSouth()) {
+            
+        }
+    }
+    public void faceNorth() {
+    }
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    public void reposition() {
         
     }
 }
